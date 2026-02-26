5c55982574eb4e057df0a50d5ad6ae9c078aabed
Krupkolllia <krupkoilla397@gmail.com>
2026-02-26 12:52:03 +0100

    Implement Circle model

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -1,4 +1,17 @@
 package core.basesyntax;
 
-public class Circle {
+public class Circle extends Figure {
+    private final double radius;
+
+    public Circle(String color, double area, double radius) {
+        super(color, area);
+        this.radius = radius;
+    }
+
+    @Override
+    public String draw() {
+        return "Figure: circle, area: " + area
+                + " sq. units, radius: " + radius
+                + " color: " + color;
+    }
 }
--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -2,9 +2,11 @@ package core.basesyntax;
 
 public abstract class Figure {
     protected String color;
+    protected double area;
 
-    public Figure(String color) {
+    public Figure(String color, double area) {
         this.color = color;
+        this.area = area;
     }
 
     public abstract String draw();
